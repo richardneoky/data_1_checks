2d4ea9fc95b719ff99c0b32c6c79237f2413de82
Richard Inman <91326618+richardneoky@users.noreply.github.com>
2023-02-21 16:48:29 -0500

    Add files via upload

---
+++ knowledgecheck.py
@@ -0,0 +1,22 @@
+print("hello world!")
+
+
+x = list(('orange', 'apple', 'watermelon'))
+print(x)
+
+d={}
+
+d[('Antje','place')]='Barcelona'
+
+d[('Antje','year')]='1987'
+
+d[('Richard','place')]='Berlin'
+
+key=('Antje','year')
+
+if key in d:
+
+    print( d[key] )
+
+    my_tuple = (1, 2, 3, 4)
+print(my_tuple)
